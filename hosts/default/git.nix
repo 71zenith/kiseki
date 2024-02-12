be17d0850c8be4a57b5d851805fa0b0b2833593f
zen <71zenith@proton.me>
2024-02-12 23:52:44 +0530

    add: git aliases

---
+++ hosts/default/git.nix
@@ -0,0 +1,37 @@
+{
+  programs.git = {
+    enable = true;
+    userName = "zen";
+    userEmail = "71zenith@proton.me";
+    aliases = {
+      co = "checkout";
+      cd = "clone --depth=1";
+      c = "commit -m";
+      w = "switch";
+      ca = "commit --amend";
+      cn = "commit --amend --no-edit";
+      a = "add -v";
+      r = "restore";
+      v = "revert";
+      va = "revert --abort";
+      pl = "pull --rebase";
+      l = "log";
+      cm = "checkout master";
+      f = "reset --hard";
+      rs = "restore --staged";
+      lp = "log -p";
+      ls = "log --stat";
+      s = "status";
+      q = "stash";
+      qa = "stash apply";
+      qc = "stash clear";
+      p = "push -v";
+      d = "diff";
+      dc = "diff --cached";
+      b = "rebase";
+      ba = "rebase --abort";
+      bc = "rebase --continue";
+      cl = "clone";
+    };
+  };
+}
