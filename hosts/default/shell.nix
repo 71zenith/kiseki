af7d41a90e1bfd3abcbfea680671a3d7da64f5ff
zen <71zenith@proton.me>
2024-02-22 21:50:54 +0530

    zsh part 2

--- hosts/default/shell.nix
+++ hosts/default/shell.nix
@@ -17,8 +17,8 @@
       historySubstringSearch = { enable = true; };
       history.size = 10000000;
       shellAliases = {
-        up = "sudo nixos-rebuild switch --flake ~/nix#default";
-        del = "sudo nix-collect-garbage --delete-old";
+        up = "sudo nixos-rebuild switch --flake ~/nix#default -j 6";
+        del = "sudo nix-collect-garbage --delete-old && nix-collect-garbage --delete-old";
         pm = "pulsemixer";
         cat = "bat -p -P";
         g = "git";
@@ -59,5 +59,103 @@
       enable = true;
       enableZshIntegration = true;
     };
+  };
+  home.file = {
+    ".p10k.zsh".text = ''
+    'builtin' 'local' '-a' 'p10k_config_opts'
+    [[ ! -o 'aliases'         ]] || p10k_config_opts+=('aliases')
+    [[ ! -o 'sh_glob'         ]] || p10k_config_opts+=('sh_glob')
+    [[ ! -o 'no_brace_expand' ]] || p10k_config_opts+=('no_brace_expand')
+    'builtin' 'setopt' 'no_aliases' 'no_sh_glob' 'brace_expand'
+
+    () {
+      emulate -L zsh -o extended_glob
+
+      unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
+
+      [[ $ZSH_VERSION == (5.<1->*|<6->.*) ]] || return
+
+      local grey='242'
+      local red='#FF5C57'
+      local yellow='#F3F99D'
+      local blue='#57C7FF'
+      local magenta='#FF6AC1'
+      local cyan='#9AEDFE'
+      local white='#F1F1F0'
+
+      typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+        dir                       # current directory
+        vcs                       # git status
+        prompt_char               # prompt symbol
+        )
+
+        typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+          command_execution_time    # previous command duration
+          virtualenv                # python virtual environment
+          context                   # user@host
+          )
+
+          typeset -g POWERLEVEL9K_BACKGROUND=                            # transparent background
+          typeset -g POWERLEVEL9K_{LEFT,RIGHT}_{LEFT,RIGHT}_WHITESPACE=  # no surrounding whitespace
+          typeset -g POWERLEVEL9K_{LEFT,RIGHT}_SUBSEGMENT_SEPARATOR=' '  # separate segments with a space
+          typeset -g POWERLEVEL9K_{LEFT,RIGHT}_SEGMENT_SEPARATOR=        # no end-of-line symbol
+          typeset -g POWERLEVEL9K_VISUAL_IDENTIFIER_EXPANSION=           # no segment icons
+
+          typeset -g POWERLEVEL9K_PROMPT_ADD_NEWLINE=false
+
+          typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS}_FOREGROUND=$magenta
+          typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS}_FOREGROUND=$red
+          typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='λ'
+          typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='Λ'
+          typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='Λ'
+          typeset -g POWERLEVEL9K_PROMPT_CHAR_OVERWRITE_STATE=false
+
+          typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=$grey
+          typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
+          typeset -g POWERLEVEL9K_VIRTUALENV_{LEFT,RIGHT}_DELIMITER=
+
+            typeset -g POWERLEVEL9K_DIR_FOREGROUND=$blue
+
+            typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE="%F{$white}%n%f%F{$grey}@%m%f"
+            typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE="%F{$grey}%n@%m%f"
+            typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_CONTENT_EXPANSION=
+
+              typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=5
+              typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=0
+              typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FORMAT='d h m s'
+              typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=$yellow
+              typeset -g POWERLEVEL9K_VCS_FOREGROUND=$grey
+
+              typeset -g POWERLEVEL9K_VCS_LOADING_TEXT=
+
+                typeset -g POWERLEVEL9K_VCS_MAX_SYNC_LATENCY_SECONDS=0
+
+                typeset -g POWERLEVEL9K_VCS_{INCOMING,OUTGOING}_CHANGESFORMAT_FOREGROUND=$cyan
+                typeset -g POWERLEVEL9K_VCS_GIT_HOOKS=(vcs-detect-changes git-untracked git-aheadbehind)
+                typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=
+                  typeset -g POWERLEVEL9K_VCS_COMMIT_ICON='@'
+                  typeset -g POWERLEVEL9K_VCS_{STAGED,UNSTAGED,UNTRACKED}_ICON=
+                    typeset -g POWERLEVEL9K_VCS_DIRTY_ICON='*'
+                    typeset -g POWERLEVEL9K_VCS_INCOMING_CHANGES_ICON=':⇣'
+                    typeset -g POWERLEVEL9K_VCS_OUTGOING_CHANGES_ICON=':⇡'
+                    typeset -g POWERLEVEL9K_VCS_{COMMITS_AHEAD,COMMITS_BEHIND}_MAX_NUM=1
+                    typeset -g POWERLEVEL9K_VCS_CONTENT_EXPANSION=''${''${''${P9K_CONTENT/⇣* :⇡/⇣⇡}// }//:/ }
+
+                    typeset -g POWERLEVEL9K_TIME_FOREGROUND=$grey
+                    typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
+                    typeset -g POWERLEVEL9K_TIME_UPDATE_ON_COMMAND=false
+
+                    typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=off
+
+                    typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
+                    typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
+                    (( ! $+functions[p10k] )) || p10k reload
+                  }
+
+                  typeset -g POWERLEVEL9K_CONFIG_FILE=''${''${(%):-%x}:a}
+
+                  (( ''${#p10k_config_opts} )) && setopt ''${p10k_config_opts[@]}
+                  'builtin' 'unset' 'p10k_config_opts'
+                  '';
   };
 }
