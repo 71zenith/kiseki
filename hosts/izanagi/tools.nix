5e6d644faed321049f2977e1cc602e31514bfd27
zen <71zenith@proton.me>
2024-05-05 09:53:15 +0530

    spotify-player update

--- hosts/izanagi/tools.nix
+++ hosts/izanagi/tools.nix
@@ -148,6 +148,7 @@ in {
     defaultTimeout = 5000;
     maxIconSize = 128;
     borderSize = 3;
+    format = ''<span foreground="#${palette.base0B}"><b><i>%s</i></b></span>\n<span foreground="#${palette.base0C}">%b</span>'';
     borderRadius = 10;
     padding = "10";
     width = 330;
