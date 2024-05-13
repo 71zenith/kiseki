b142fccfaca8250e15fa047f77407b44bd637094
zen <71zenith@proton.me>
2024-05-13 09:24:22 +0530

    move nvim-plugins to another file

--- hosts/izanagi/hm/nvim.nix
+++ hosts/izanagi/hm/nvim.nix
@@ -1,46 +1,10 @@
 {
   inputs,
-  pkgs,
   config,
+  pkgs,
   ...
 }: let
-  # HACK: manually defining till official package
-  lualine-so-fancy = pkgs.vimUtils.buildVimPlugin {
-    name = "lualine-so-fancy";
-    src = pkgs.fetchFromGitHub {
-      owner = "meuter";
-      repo = "lualine-so-fancy.nvim";
-      rev = "21284504fed2776668fdea8743a528774de5d2e1";
-      hash = "sha256-JMz3Dv3poGoYQU+iq/jtgyHECZLx+6mLCvqUex/a0SY=";
-    };
-  };
-  buffer-manager = pkgs.vimUtils.buildVimPlugin {
-    name = "buffer-manager";
-    src = pkgs.fetchFromGitHub {
-      owner = "j-morano";
-      repo = "buffer_manager.nvim";
-      rev = "fd36131b2b3e0f03fd6353ae2ffc88cf920b3bbb";
-      hash = "sha256-abe9ZGmL7U9rC+LxC3LO5/bOn8lHke1FCKO0V3TZGs0=";
-    };
-  };
-  nvim-paredit = pkgs.vimUtils.buildVimPlugin {
-    name = "nvim-paredit";
-    src = pkgs.fetchFromGitHub {
-      owner = "julienvincent";
-      repo = "nvim-paredit";
-      rev = "8dd4ffd6ee0d798026f9ad6cf04344560207b9f1";
-      hash = "sha256-Zo40MOBSkLFSaK+x6iiNXhV9c/yNCi2jckl5VOpBDU8=";
-    };
-  };
-  img-clip = pkgs.vimUtils.buildVimPlugin {
-    name = "img-clip";
-    src = pkgs.fetchFromGitHub {
-      owner = "HakonHarnes";
-      repo = "img-clip.nvim";
-      rev = "c55f988972be0027316889c57ffe49f2bf8f67fd";
-      hash = "sha256-sl//iua0S4hdulopAjOOe7xh/iEuQeNRlNECxcc6rXk=";
-    };
-  };
+  plugins = import ../../../pkgs/nvim-plugins.nix {inherit pkgs;};
 in {
   imports = [
     ../../../modules/hm/neovide.nix
@@ -161,7 +125,7 @@ in {
         updatetime = 200;
         showbreak = "⤷ ";
       };
-      extraPlugins = [lualine-so-fancy buffer-manager img-clip nvim-paredit];
+      extraPlugins = with plugins; [lualine-so-fancy buffer-manager img-clip nvim-paredit];
       plugins = {
         nix.enable = true;
         hop.enable = true;
