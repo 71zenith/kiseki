d8eb81d0e3f6de37382a253a8b1842c051816510
zen <71zenith@proton.me>
2024-05-14 18:34:06 +0530

    hyprland workspaces refactor

--- hosts/izanagi/hm/tools.nix
+++ hosts/izanagi/hm/tools.nix
@@ -173,15 +173,18 @@ in {
       };
     };
   };
-  services.mako = {
-    enable = true;
-    defaultTimeout = 5000;
-    maxIconSize = 128;
-    borderSize = 3;
-    format = ''<span foreground="#${palette.base0B}"><b><i>%s</i></b></span>\n<span foreground="#${palette.base0C}">%b</span>'';
-    borderRadius = 10;
-    padding = "10";
-    width = 330;
-    height = 200;
+  services = {
+    blueman-applet.enable = true;
+    mako = {
+      enable = true;
+      defaultTimeout = 5000;
+      maxIconSize = 128;
+      borderSize = 3;
+      format = ''<span foreground="#${palette.base0B}"><b><i>%s</i></b></span>\n<span foreground="#${palette.base0C}">%b</span>'';
+      borderRadius = 10;
+      padding = "10";
+      width = 330;
+      height = 200;
+    };
   };
 }
