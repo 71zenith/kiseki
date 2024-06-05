6a557d72fc5d9c4c9ed6b289a1de329275de80f2
zen <71zenith@proton.me>
2024-06-05 22:44:35 +0530

    more emacs update

--- flake.nix
+++ flake.nix
@@ -81,7 +81,7 @@
     };
   in {
     devShell.${system} = pkgs.mkShell {
-      packages = with pkgs; [lolcat];
+      packages = with pkgs; [lolcat alejandra];
       shellHook = ''
         printf "\e[3m\e[1m%s\em\n" "1337 h4x0ring..." | lolcat
       '';
