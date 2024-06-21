4564903d14dcc64a40bd082e50ad7519e2cb856d
zen <71zenith@proton.me>
2024-06-21 10:30:02 +0530

    move env to home than hyprland (river incoming)

--- hosts/izanagi/hm/hyprland.nix
+++ hosts/izanagi/hm/hyprland.nix
@@ -23,25 +23,6 @@ in {
           workspace_method = "first 1";
         };
       };
-      env = [
-        "LIBVA_DRIVER_NAME,nvidia"
-        "XDG_SESSION_TYPE,wayland"
-        "GBM_BACKEND,nvidia-drm"
-        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-        "WLR_NO_HARDWARE_CURSORS,1"
-        "NIXOS_OZONE_WL,1"
-        "MOZ_ENABLE_WAYLAND,1"
-        "MOZ_WEBRENDER,1"
-        "_JAVA_AWT_WM_NONREPARENTING,1"
-        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-        "QT_QPA_PLATFORM,wayland;xcb"
-        "QT_QPA_PLATFORMTHEME,gtk3"
-        "ELECTRON_OZONE_PLATFORM_HINT,wayland"
-        "DISABLE_QT5_COMPAT,0"
-        "CALIBRE_USE_DARK_PALETTE,1"
-        "SDL_VIDEODRIVER,wayland"
-        "GDK_BACKEND,wayland"
-      ];
       "$mod1" = "SUPER";
       "$mod2" = "SUPERSHIFT";
       "$mod3" = "SUPERCONTROL";
