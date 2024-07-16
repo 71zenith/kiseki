e1de34bf5f86558a218f015676a4ed6ffd90c672
zen <71zenith@proton.me>
2024-07-16 13:45:03 +0530

    overlay update

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,6 +1,6 @@
 self: super: {
   spotify-player = self.callPackage ./spotify-player.nix {};
   yazi-plugins = self.callPackage ./yazi-plugins.nix {};
-  fcitx5-fluent = self.callPackage ./fcitx-fluent.nix {};
+  fcitx5-fluent = self.callPackage ./fcitx5-fluent.nix {};
   ani-cli = self.callPackage ./ani-cli.nix {};
 }
