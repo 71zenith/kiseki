7d6897bd6d8d9371211da7d4a9c61909e29ad2f5
zen <71zenith@proton.me>
2024-07-23 20:28:23 +0530

    .

--- home/gui.nix
+++ home/gui.nix
@@ -10,7 +10,6 @@ in {
     zathura.enable = false;
   };
 
-  # TODO: downgrade the package below 5.1.9
   # i18n.inputMethod = {
   #   enabled = "fcitx5";
   #   fcitx5.addons = with pkgs; [fcitx5-mozc fcitx5-gtk fcitx5-fluent];
