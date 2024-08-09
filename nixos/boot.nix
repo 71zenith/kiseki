2dcb5d605d2b1569ebdad7919a6cb1cc03d58cea
zen <71zenith@proton.me>
2024-08-09 18:48:41 +0530

    add games drive to fstab

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,6 +1,7 @@
 {
   lib,
   modulesPath,
+  myUserName,
   ...
 }: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
@@ -11,16 +12,23 @@
     kernelModules = ["kvm-amd"];
     extraModulePackages = [];
   };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/a712d0ab-a784-4a07-bcfa-c538173722c2";
+      fsType = "btrfs";
+      options = ["subvol=@" "compress=zstd"];
+    };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/a712d0ab-a784-4a07-bcfa-c538173722c2";
-    fsType = "btrfs";
-    options = ["subvol=@" "compress=zstd"];
-  };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/65F1-EC95";
+      fsType = "vfat";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/65F1-EC95";
-    fsType = "vfat";
+    "/home/${myUserName}/mnt" = {
+      device = "/dev/disk/by-uuid/d574db64-7098-4a82-8837-3f4f43f3b003";
+      fsType = "ext4";
+      options = ["noauto"];
+    };
   };
 
   swapDevices = [{device = "/dev/disk/by-uuid/2d4b4ca5-eabb-4ded-ac0c-b26bfec0d36a";}];
