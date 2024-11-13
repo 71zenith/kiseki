5c18100ad8daa6a612c706fca9df0bbc86d5e31c
zen <71zenith@proton.me>
2024-11-13 19:11:46 +0530

    flake: update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "crossbell cathedral";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nix-colors.url = "github:misterio77/nix-colors";
     home-manager = {
